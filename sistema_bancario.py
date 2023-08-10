58bbcfae97b2d1ee533a83670d9c542ee7274063
marcostiagofh <marcostiagofh@gmail.com>
2023-08-10 13:53:21 -0300

    funcionalidade de saque e extrato

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,6 +1,7 @@
 import re
 
 regex_pattern_deposito = r'^\d+\.\d{2}$'
+regex_pattern_saque = r'^\d+$'
 
 menu = """
 
@@ -36,11 +37,30 @@ while True:
 			extrato += registro_deposito.format(valor_deposito, saldo)
 			print(registro_deposito.format(valor_deposito, saldo))       			
 		case "S":
-			print("Sacar")				
+			print("Sacar")		
+			if numero_saques == LIMITE_SAQUES:
+				print("limite diario de saques excedido (%d)",LIMITE_SAQUES)
+			else:
+				valor_saque = input
+				valor_validado = False
+				while not valor_validado:
+					valor_saque_str = input("Por favor, informe o valor de depósito no formato XXXXX :")
+					if re.match(regex_pattern_saque,valor_saque_str):
+						valor_saque = int(valor_deposito_str)
+						valor_validado = True
+						if valor_saque > 500:
+							print("Valor acima do limite permitido (500)")
+							valor_validado = False
+							
+				numero_saques += 1
+				saldo -= valor_saque
+				registro_deposito = "\nSaque {}\nSaldo {}"
+				extrato += registro_deposito.format(valor_saque, saldo)
+				print(registro_deposito.format(valor_saque, saldo))				
 						
 		case "E":
 			print("Extrato")
-            
+			print(extrato)            
 		case "Q":
 			print("Sair")		
 			exit();
